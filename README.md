0508edbf070e5f5b60309e2566430bf74b70ba3d
emoacht <emotom@pobox.com>
2024-08-17 12:11:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WPF Fixed Test
+
+Test a window whose size is fixed regardless of the DPI of a monitor except non-client area (title bar).
---
+++ WpfFixedTest/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace WpfFixedTest;
+
+public partial class MainWindow : Window
+{
+	private readonly HwndSource _source;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		var handle = new WindowInteropHelper(this).EnsureHandle();
+		_source = HwndSource.FromHwnd(handle);
+		_source.AddHook(WndProc);
+	}
+
+	public DpiScale FixedDpi { get; } = new DpiScale(1, 1);
+
+	protected override void OnSourceInitialized(EventArgs e)
+	{
+		base.OnSourceInitialized(e);
+
+		var initialDpi = VisualTreeHelper.GetDpi(this);
+		Debug.WriteLine($"Initial DPI: {initialDpi.PixelsPerDip}");
+		var (factorX, factorY) = (FixedDpi.DpiScaleX / initialDpi.DpiScaleX, FixedDpi.DpiScaleY / initialDpi.DpiScaleY);
+
+		var content = this.Content as FrameworkElement;
+		if (content is not null)
+		{
+			content.LayoutTransform = new ScaleTransform(factorX, factorY);
+		}
+	}
+
+	private const int WM_DPICHANGED = 0x02E0;
+
+	private nint WndProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled)
+	{
+		if (msg is WM_DPICHANGED)
+		{
+			Debug.WriteLine("DPI Changed");
+			handled = true;
+		}
+		return IntPtr.Zero;
+	}
+
+	protected override void OnClosed(EventArgs e)
+	{
+		_source?.RemoveHook(WndProc);
+
+		base.OnClosed(e);
+	}
+}
